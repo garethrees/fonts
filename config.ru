8da356b732dfc3fd7cbb74e485056d350c95bb16
Gareth Rees <gareth.h.rees@gmail.com>
2012-11-22 21:46:52 +0000

    Tidy config.ru and add config

--- config.ru
+++ config.ru
@@ -1,12 +1,8 @@
 require 'rubygems'
-require 'sinatra'
 require 'bundler'
-require 'erb'
-require 'fistface'
 
 Bundler.require
 
 require './fonts'
-$stdout.sync = true
 
-run Fonts.new
+run Fonts
--- fonts.rb
+++ fonts.rb
@@ -3,9 +3,23 @@ require 'fistface'
 
 class Fonts < Sinatra::Base
   register Sinatra::FistFace
-  set :root, File.dirname(__FILE__)
+
+  configure do
+    set :root, File.dirname(__FILE__)
+    set :app_file, __FILE__
+  end
+
+  configure :development do
+    enable :logging, :dump_errors, :raise_errors
+  end
+
+  configure :production do
+    set :raise_errors, false
+    set :show_exceptions, false
+  end
 
   get '/' do
     erb :index
   end
+
 end
